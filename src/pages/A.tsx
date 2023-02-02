36e1eba84cfdf0697eee4c2e7ccf9e89b3a3d6d0
roberto <roberto.almanza05@gmail.com>
2023-02-01 18:55:41 -0700

    kkk

--- src/pages/A.tsx
+++ src/pages/A.tsx
@@ -1,41 +1,63 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function A() {
+  const [images, setImages] = useState<Array<string>>([]);
 
-    const [images, setImages] = useState<Array<string>>([])
+  useEffect(() => {
+    const xd = () => {
+      if (images.length > 1) return () => {};
 
-    useEffect(() => {
-        if(images.length>29 )return ()=> {}
+      const test = [];
 
-        const test= []
-        
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({})
-        };
-        
-        fetch('https://api.waifu.pics/many/sfw/waifu', requestOptions)
-            .then(response => response.json())
-            .then(data => setImages(data?.files));
-        
-        
-    }, [images.length]);
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({}),
+      };
 
-    
+      fetch("https://api.waifu.pics/many/sfw/waifu", requestOptions)
+        .then((response) => response.json())
+        .then((data) => setImages(data?.files));
+    };
+    xd();
+  }, [images.length]);
+
   return (
-    <div>
-    
-    
-    {images.map((image,index) => {
-            return (
-            <img style={{width:"300px", height:"300px"}} key={index} 
-            src={`${image}`}></img> 
+    <div style={{ backgroundColor: "black" }}>
+<h1 style={{color:"green", display:"flex",justifyContent:"center"}}>Hola</h1>
 
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "red",
+            width: "70%",
+            justifyContent: "center",
+            display: "grid",
+            gridTemplateColumns: ".8fr .8fr .8fr .8fr ",
+            gridGap: "10px",
+          }}
+        >
+          {images.map((image, index) => {
+            return (
+              <img
+                style={{
+                  width: "300px",
+                  height: "300px",
+                  borderRadius: "40px",
+                  marginLeft: "1%",
+                }}
+                key={index}
+                src={`${image}`}
+              ></img>
             );
           })}
+        </div>
+      </div>
     </div>
-    
-  )
-
+  );
 }
